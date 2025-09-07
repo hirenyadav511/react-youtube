9a71d53edb41efe30bd723998777626593afed33
hirenyadav511 <hirenyadav2004@gmail.com>
2025-09-07 23:25:14 +0530

    Setup Context API: create and provide shared state across components

---
+++ 116_usecontext/src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import Component1 from "./Component1";
+import { counterContext } from "../context/context";
+
+const Button = () => {
+  const value = useContext(counterContext);
+
+  return (
+    <div>
+      <button onClick={() => value.setCount((count) => count + 1)}>
+        <span>
+          <Component1 />
+        </span>
+        i am a button
+      </button>
+    </div>
+  );
+};
+
+export default Button;
